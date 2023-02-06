8800a1e38b10c86ff0e1e3df89f842627dd231ad
Govindrajawat <govindrajawat260398@gmail.com>
2023-02-06 23:17:54 +0530

    background-changes

---
+++ script.js
@@ -0,0 +1,23 @@
+let background = document.getElementById('background');
+let title = document.getElementById('title');
+let button = document.getElementsById('button');
+
+function rosegoldColor() {
+  background.style.backgroundColor = '#57D1C9';
+  title.style.color = '#57D1C9';
+}
+
+function coralColor() {
+  background.style.backgroundColor = '#518f8b';
+  title.style.color = '#FF7A6C';
+}
+
+function fuschiaColor() {
+  background.style.backgroundColor = '#A24868';
+  title.style.color = '#f05945';
+}
+
+function blackColor() {
+  background.style.backgroundColor = '#373449';
+  title.style.color = '#ED5485';
+}
